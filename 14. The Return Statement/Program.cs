a76f7c4d1cd6d881be31bdeb8feab706c0807ed4
user <user@LAPTOP-KAN0NU4I>
2023-09-14 22:14:00 +0200

    Add project files.

---
+++ 14. The Return Statement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.The_Return_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The cube of the number you entered is ");
+            Console.WriteLine(Cube(5));
+
+            //Freeze console
+        }
+
+        static int Cube(int x)
+        {
+            return x*x*x; 
+        }
+    }
+}
